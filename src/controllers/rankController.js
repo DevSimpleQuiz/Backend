b21e0567cef3b557e7a3feae275c99d51341bf43
Jake1152 <rancho974@gmail.com>
2024-09-05 11:41:48 +0900

    feat: 랭킹 api 분리(top, near, my)

--- src/controllers/rankController.js
+++ src/controllers/rankController.js
@@ -7,18 +7,24 @@ const {
 } = require("../services/rankService.js");
 const scoreQuery = require("../queries/scoreQuery.js");
 
-/**
- * 랭킹 정보에 대한 응답 제공
- *
- * 0. 스코어 정보 가져오기
- * 1. 나의 랭킹 정보 구성(id, rank, total score, quiz count, sovled count)
- * 2. top3 랭커 정보 구성
- *   - top3인 유저의 id 정보 가져오기
- * 3. near3 랭커 정보 구성
- */
-const rankInfo = async (req, res, next) => {
+const topRankersInfo = async (req, res, next) => {
   try {
-    let result = {};
+    const queryResult = await pool.query(scoreQuery.getAllrankInfo);
+
+    //  0. 스코어 정보 가져오기
+    const scoreInfos = queryResult[0];
+
+    // 1. top3 랭커 정보
+    const result = await topThreeRankerInfo(scoreInfos);
+    return res.json(result);
+  } catch (err) {
+    console.error("topRankersInfo err : ", err);
+    next(err);
+  }
+};
+
+const myRankInfo = async (req, res, next) => {
+  try {
     const queryResult = await pool.query(scoreQuery.getAllrankInfo);
 
     const token = req.cookies.token;
@@ -29,25 +35,43 @@ const rankInfo = async (req, res, next) => {
     const scoreInfos = queryResult[0];
 
     // 1. 나의 랭킹 정보 구성(id, rank, total score, quiz count, sovled count)
-    result = await getMyRankInfo(scoreInfos, userId);
-
-    // 2. top3 랭커 정보 구성
-    result = { ...result, ...(await topThreeRankerInfo(scoreInfos)) };
-
-    // 3. near3 랭커 정보 구성
-    const myScoreInfoIdx = result["rank"] - 1;
-    result = {
-      ...result,
-      ...(await nearThreeRankerInfo(scoreInfos, myScoreInfoIdx)),
-    };
+    const result = await getMyRankInfo(scoreInfos, userId);
 
     return res.json(result);
   } catch (err) {
-    console.error("rankInfo err : ", err);
+    console.error("myRankInfo err : ", err);
+    next(err);
+  }
+};
+
+const nearRankersInfo = async (req, res, next) => {
+  try {
+    const queryResult = await pool.query(scoreQuery.getAllrankInfo);
+
+    const token = req.cookies.token;
+    const payload = await verifyToken(token);
+    const userId = payload.id;
+
+    //  0. 스코어 정보 가져오기
+    const scoreInfos = queryResult[0];
+
+    // 1. 나의 랭킹 정보 구성(id, rank, total score, quiz count, sovled count)
+    let myRank = await getMyRankInfo(scoreInfos, userId);
+    myRank = myRank["rank"];
+
+    // 2. near3 랭커 정보 구성
+    const myScoreInfoIdx = myRank - 1;
+    result = await nearThreeRankerInfo(scoreInfos, myScoreInfoIdx);
+
+    return res.json(result);
+  } catch (err) {
+    console.error("nearRankersInfo err : ", err);
     next(err);
   }
 };
 
 module.exports = {
-  rankInfo,
+  topRankersInfo,
+  myRankInfo,
+  nearRankersInfo,
 };
--- src/routes/rank.js
+++ src/routes/rank.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const { isAuthenticated } = require("../middlewares/authMiddleware");
-const { rankInfo } = require("../controllers/rankController.js");
+const {
+  topRankersInfo,
+  myRankInfo,
+  nearRankersInfo,
+} = require("../controllers/rankController.js");
 
-router.get("/", isAuthenticated, rankInfo);
+router.get("/top", topRankersInfo);
+router.get("/my", isAuthenticated, myRankInfo);
+router.get("/near", isAuthenticated, nearRankersInfo);
 
 module.exports = router;
