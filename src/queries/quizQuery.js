f5dd3eccb709108362aa80fbcd42439c0e4fab9b
Jake1152 <rancho974@gmail.com>
2024-09-11 00:33:25 +0900

    feat: 퀴즈 채점 api

--- src/queries/quizQuery.js
+++ src/queries/quizQuery.js
@@ -1,4 +1,4 @@
-exports.insertQuiz = `INSERT INTO quiz (quiz_type, definition, initial_constant) VALUES (?, ?, ?, ?)`;
+exports.insertQuiz = `INSERT INTO quiz (quiz_type, word, definition, initial_constant) VALUES (?, ?, ?, ?)`;
 exports.getQuizIdByWord = `SELECT id FROM quiz WHERE word = ?`;
 exports.insertQuizStatistics = `INSERT INTO quiz_accuracy_statistics (quiz_id, correct_people_count, total_attempts_count_before_correct) VALUES (?, ?, ?)`;
 exports.generateQuizzes = `SELECT q.id AS quizId, q.word, q.initial_constant AS initialConstant, q.definition,
@@ -7,3 +7,5 @@ exports.generateQuizzes = `SELECT q.id AS quizId, q.word, q.initial_constant AS 
                             JOIN quiz_accuracy_statistics s ON q.id = s.quiz_id
                             ORDER BY RAND()
                             LIMIT ?`;
+
+exports.getWordQuery = `SELECT word FROM quiz WHERE id = ?`;
